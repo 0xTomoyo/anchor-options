e0d431b60d330e9eb0f0338e2704cd178e1578d3
0xTomoyo <0xtomoyo@protonmail.com>
2022-02-12 12:47:44 +1100

    First commit

---
+++ tests/anchor-options.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { AnchorOptions } from '../target/types/anchor_options';
+
+describe('anchor-options', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.AnchorOptions as Program<AnchorOptions>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
